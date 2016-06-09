b04ed9db6bf73b0d8df29609e864f8afc5f5a15c
TaylanT <tokan@ift.uni-hannover>
2016-06-09 15:53:02 +0200

    Queryset nach aktuellem Monat gefiltert

--- stundenkonto/views.py
+++ stundenkonto/views.py
@@ -5,12 +5,13 @@ import datetime
 
 # Create your views here.
 def ubersicht(request):
-	#zt=ZeitErfassung.objects.all()
-	#current_month=datetime.now().month
-	zt=ZeitErfassung.objects.filter(user__username=request.user).order_by('start')
+	heute=datetime.date.today()
+	aktueller_monat=heute.month
+	zt=ZeitErfassung.objects.filter(user__username=request.user,start__month=aktueller_monat).order_by('start')
 
-	return render(request,'uebersicht.html',{'zt':zt})
+	return render(request,'uebersicht.html',{'zt':zt,'monat':heute.strftime("%B")})
 def status(request):
+	
 	user_zeit=ZeitErfassung.objects.filter(user__username=request.user)
 
 	summe=datetime.timedelta(0)
