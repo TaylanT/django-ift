95c6aeca1667f91b2124e3af2ca1d7c23048bd77
TaylanT <tokan@ift.uni-hannover>
2016-06-14 15:08:56 +0200

    flake8 on stundenkonto

--- stundenkonto/models.py
+++ stundenkonto/models.py
@@ -4,11 +4,10 @@ from django.db import models
 
 # Create your models here.
 
-class VertragsUebersicht(models.Model):
-	Vertragsstunden=models.CharField(max_length=120)
-	Vertragsstart=models.DateField(null=True)
-	Vertragsende=models.DateField(null=True)
-#class OffeneStunden(models.Model):
 
-	
-	
+class VertragsUebersicht(models.Model):
+    Vertragsstunden = models.CharField(max_length=120)
+    Vertragsstart = models.DateField(null=True)
+    Vertragsende = models.DateField(null=True)
+    Ueberhang = models.F
+# class OffeneStunden(models.Model):
--- stundenkonto/views.py
+++ stundenkonto/views.py
@@ -1,31 +1,37 @@
 
-from django.shortcuts import render, HttpResponseRedirect
-from login.models import ZeitErfassung,MyUser
+from django.shortcuts import render
+from login.models import ZeitErfassung
 import datetime
 import locale
 
 locale.setlocale(locale.LC_ALL, 'de_DE@euro')
 
 # Create your views here.
+
+
 def ubersicht(request):
-	heute=datetime.date.today()
-	aktueller_monat=heute.month
-	zt=ZeitErfassung.objects.filter(user__username=request.user,start__month=aktueller_monat).order_by('start')
 
-	return render(request,'uebersicht.html',{'zt':zt,'monat':heute.strftime("%B")})
+    heute = datetime.date.today()
+    aktueller_monat = heute.month
+    zt = ZeitErfassung.objects.filter(user__username=request.user, start__month=aktueller_monat).order_by('start')
+
+    return render(request, 'uebersicht.html', {'zt': zt, 'monat': heute.strftime("%B")})
+
+
 def status(request):
-	heute=datetime.date.today()
-	aktueller_monat=heute.month
-	
-	user_zeit=ZeitErfassung.objects.filter(user__username=request.user,start__month=aktueller_monat)
-	#vertrag_stunden=MyUser.objects.filter(user__username=request.user)
-	summe=datetime.timedelta(0)
+    heute = datetime.date.today()
+    aktueller_monat = heute.month
 
-	for zeit in user_zeit:
-		summe=summe+zeit.dt
+    user_zeit = ZeitErfassung.objects.filter(user__username=request.user, start__month=aktueller_monat)
+    # vertrag_stunden=MyUser.objects.filter(user__username=request.user)
+    summe = datetime.timedelta(0)
 
-	summe=summe.total_seconds()/3600.0
-	return render(request,'status.html',{'summe':summe})
-  
+    for zeit in user_zeit:
+        summe = summe+zeit.dt
+
+    summe = summe.total_seconds()/3600.0
+    return render(request, 'status.html', {'summe': summe})
+
+
 def thanks(request):
-	return render(request,'thanks.html',{})
+    return render(request, 'thanks.html', {})
