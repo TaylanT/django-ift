939ff5b3eb2a6e888b0a0d98eee324e8049c681c
Taylan Tokan <tokan@ift.uni-hannover.de>
2016-06-30 01:55:50 +0200

    update record hinzugefügt

--- stundenkonto/models.py
+++ stundenkonto/models.py
@@ -19,14 +19,28 @@ from django.db.models import F, FloatField, Sum
 class StatusUebersicht(models.Model):
     """Ueberblick uber aktuellen stundenstatus nach MTC."""
     Monatsstunden = models.FloatField()
+    User = models.ForeignKey('login.MyUser', on_delete=models.CASCADE,
+    )
     
+    def __str__(self): # Python 3.3 is __str__
+        return '%s' %(self.User)
+
     def berechnen(self, request):
         heute = datetime.date.today()
         aktueller_monat = heute.month
         zeit = ZeitErfassung.objects.filter(user__username=request.user,
                                  start__month=aktueller_monat).aggregate(test=Sum(F('dt')))
+        udi=request.user
+        udi.id
+        t = StatusUebersicht.objects.get(User_id=request.user.id)
+        
         zeit_stunden = zeit['test'].total_seconds()/3600
-        self.Monatsstunden = zeit_stunden
-        self.save()
+        #self.Monatsstunden = zeit_stunden
+        t.User = request.user
+        t.Monatsstunden = zeit_stunden  # change field
+        t.save() # this will update only
+        # TODO nonetype abfang hinzufuegen fuer erreichen status auch ohne eintrag
+
+        
         return zeit_stunden
 
