fdb0c36d06de3739e612cfcc4e0219ca7d44245f
Taylan Tokan <tokan@ift.uni-hannover.de>
2016-08-05 13:42:23 +0200

    vorhandene monatsleiste eingefugt- layout muss noch gemacht werden

--- stundenkonto/views.py
+++ stundenkonto/views.py
@@ -25,26 +25,40 @@ class UebersichView(ListView):
 
     def get_queryset(self, *args, **kwargs):
         """"Angepasste Queryset."""
-        if(self.kwargs != {}):
-            monat = self.kwargs['monat']
-            print(monat)
-        else:
-            heute = datetime.date.today()
-            monat = heute.month
+        monat = self.kwargs['monat']
+        # if(self.kwargs != {}):
+        #     monat = self.kwargs['monat']
+        #     print(monat)
+        # else:
+        #     heute = datetime.date.today()
+        #     monat = heute.month
         return ZeitErfassung.objects.filter(user__username=self.request.user,
                                             start__month=monat).order_by('start')
 
     def get_context_data(self, **kwargs):
         """Erweitererung contexdata."""
-        #heute = datetime.date.today()
+        # heute = datetime.date.today()
         context = super(UebersichView, self).get_context_data(**kwargs)
-        #context['monat'] = heute.strftime("%B")
+        # context['monat'] = heute.strftime("%B")
         if(self.kwargs != {}):
             monat = self.kwargs['monat']
         else:
-            monat =  datetime.date.today().month
+            monat = datetime.date.today().month
         context['monat'] = monat
+
+        monatsliste = []
+        for x in range(1, 12):
+            zt = ZeitErfassung.objects.filter(user__username='taylan',
+                                              start__month=x)
+            if zt.count() > 0:
+                monatsliste.append(x)
+            else:
+                pass
+        context['monatslist'] = monatsliste
         return context
+
+
+
 
 
 def status(request):
@@ -53,7 +67,7 @@ def status(request):
     summe = test.berechnen(request)
     ueberhang = test.ueberhang(request)
     # summeNeu = summe/ueberhang
-    return render(request, 'status.html', { 'summe': summe, 
+    return render(request, 'status.html', {'summe': summe, 
                                            'ueberhang': ueberhang,
                                            'monat': StatusUebersicht().get_aktuellermonat(),
                                            'Vertragsstunden': MyUser.objects.get(username=request.user).Vertragstunden
